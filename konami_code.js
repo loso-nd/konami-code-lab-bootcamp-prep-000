48d8ab7e62838c4b449819a973a0247b5b9d0511
Nahomie ND <nducasse3@gmail.com>
2020-12-06 01:38:19 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,25 +1,68 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
 function init() {
-  // Write your JavaScript code inside the init() function
-  // Keep track of index outside of the event handler.
-  let index = 0;
-  const body = document.getElementsByTagName('body')[0];
 
-  body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.detail || e.which);
-    console.log(key);
-
-  if (key === code[index]) {
-      index++;
-
-      if (index === code.length) {
-        alert("Hurray!");
-
-        index = 0;
-      }
-    } else {
-      index = 0;
+  let i = 0
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.which
+     
+    if (code[i] === key) {
+        i++;
+        
+      if(i === code.length){
+        alert('Congrats! My Guy!');
+          i=0;
+          
+        }
+      } 
+      else {
+          i=0; 
     }
   });
 }
+
+init();
+
+
+
+
+
+
+// const code = [  "ArrowUp", "ArrowUp",  "ArrowDown", "ArrowDown",  "ArrowLeft", "ArrowRight",  "ArrowLeft", "ArrowRight",  "b",  "a"];
+
+
+// function init() 
+
+// {let index = 0;  document.body.addEventListener("keydown", function(e) {
+//   const key = e.key;   if (code[index] === key) {
+// index++;     if (index === code.length) {
+//   alert("Hurray!");
+// index = 0;
+// }
+// } else {
+//   index = 0;
+//   }
+// });
+  
+// }
+// init();
+  
+  // let index = 0;
+  // const body = document.getElementsByTagName('body')[0];
+
+  // body.addEventListener('keydown', function(e) {
+  //   const key = parseInt(e.detail || e.which);
+  //   console.log(key);
+
+  // if (key === code[index]) {
+  //     index++;
+
+  //     if (index === code.length) {
+  //       alert("Hurray!");
+
+  //       index = 0;
+  //     }
+  //   } else {
+  //     index = 0;
+  //   }
+  // });
+// }
