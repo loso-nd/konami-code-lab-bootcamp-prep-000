27706f9f7b1df21c66a98eb32a44d2331ed403a1
Nahomie ND <nducasse3@gmail.com>
2020-12-06 01:49:39 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,12 +2,12 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
 
-  let i = 0
+  let i = 0;
   const body = document.getElementsByTagName('body');
   
   document.body.addEventListener('keydown', function(e) {
      
-    if (e.which === code[i]) {
+    if (e.key === code[i]) {
         i++;
         
       if(i === code.length){
