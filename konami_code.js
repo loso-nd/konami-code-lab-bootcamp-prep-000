fae59a8e97f1f9f1a8d2a9f53e359b4d1946e443
Nahomie ND <nducasse3@gmail.com>
2020-12-06 02:34:24 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,26 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
 
-  let i = 0;
+  let index = 0;
+
+body.addEventListener('keydown', function(e) {
+  const key = parseInt(e.detail || e.which);
+  console.log(key);
   
+  if (key === code[index]) {    
+    index++;
+    if (index === code.length) {
+      alert("Hurray!");
+      index = 0;
+      }
+    } else {     
+      index = 0;
+      }
+    });
+  
+}
+
+
 //   const body = document.getElementsByTagName('body')[0];
   
 //   body.addEventListener('keydown', function(e) {
@@ -29,7 +47,3 @@ function init() {
 //     }
 //   });
 // }
-
-   body.addEventListener('keydown', function(e) {     const key = parseInt(e.detail || e.which);     console.log(key);    if (key === code[index]) {       index++;        if (index === code.length) {         alert("Hurray!");          index = 0;       }     } else {       index = 0;     }   }); } 
-
-
