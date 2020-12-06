7b7020c4e46afaaa1b294f928526bf430ab12acf
Nahomie ND <nducasse3@gmail.com>
2020-12-06 01:42:19 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -23,28 +23,6 @@ function init() {
 
 init();
 
-  // let index = 0;
-  // const body = document.getElementsByTagName('body')[0];
-
-  // body.addEventListener('keydown', function(e) {
-  //   const key = parseInt(e.detail || e.which);
-  //   console.log(key);
-
-  // if (key === code[index]) {
-  //     index++;
-
-  //     if (index === code.length) {
-  //       alert("Hurray!");
-
-  //       index = 0;
-  //     }
-  //   } else {
-  //     index = 0;
-  //   }
-  // });
-// }
-
-
 
 
 
